dc4645a870285f6a2688afa2942099938a99f9c2
koomchang <leahpar0401@gmail.com>
2023-02-13 21:25:11 +0900

    chore: DockerFile, Docker-compose 생성

---
+++ webapp/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python manage.py makemigrations --noinput
+
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+
+gunicorn config.wsgi:application --env DJANGO_SETTINGS_MODULE=config.settings.local --bind 0.0.0.0:8000
