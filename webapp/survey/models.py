f686aec91b165932b1b792adb2604bde5ba03550
koomchang <leahpar0401@gmail.com>
2023-03-19 22:44:57 +0900

    feat: survey 모델 생성

--- webapp/survey/models.py
+++ webapp/survey/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from config.baseModel import BaseModel
 
-# Create your models here.
+
+class Survey(BaseModel):
+    IDLE = 0
+    ONGOING = 1
+    DONE = 2
+    AWARDED = 3
+
+    STATUS = (
+        (IDLE, 'idle'),
+        (ONGOING, 'ongoing'),
+        (DONE, 'done'),
+        (AWARDED, 'awarded'),
+    )
+
+    title = models.CharField(
+        max_length=255,
+        null=False,
+    )
+
+    thumbnail = models.URLField(
+        max_length=255,
+    )
+
+    data = models.JSONField(
+        null=False,
+    )
+
+    status = models.IntegerField(
+        verbose_name="설문 상태",
+        null=False,
+        choices=STATUS,
+        default=IDLE,
+    )
+
+    is_survey_hidden = models.BooleanField(
+        verbose_name="공개_비공개_여부",
+        null=False,
+        default=False,
+    )
+
+    end_at = models.DateTimeField(
+        verbose_name="설문 종료 일시",
+        null=False,
+    )
