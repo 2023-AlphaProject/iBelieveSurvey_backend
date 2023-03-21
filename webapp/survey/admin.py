7c2b439fe333a312b217d5257ae78ffcd4cbef74
koomchang <leahpar0401@gmail.com>
2023-03-21 19:03:03 +0900

    feat: survey 앱 admin

--- webapp/survey/admin.py
+++ webapp/survey/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Survey
+from survey.models import Survey, Category
 
-# Register your models here.
-admin.site.register(Survey)
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'status', 'end_at', 'created_at')
+    list_filter = ('category', 'status', 'created_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type')
