39af96fe03609a5a92bbb3206cac39cf334e9f97
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-03-22 11:21:36 +0900

    fix: correct swagger url convention

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('surveys', include(('survey.urls', 'api'))),
+    path('surveys/', include(('survey.urls', 'api'))),
 ]
 
 if settings.DEBUG:
