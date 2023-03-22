f1c29eb7f7e9e40ecfa9111a9be98a72e0398200
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-03-22 23:16:32 +0900

    feat: user model 생성

---
+++ webapp/user/models/user.py
@@ -0,0 +1,44 @@
+from django.db import models
+from config.baseModel import BaseModel
+
+class User(BaseModel):
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    kakao_id = models.CharField(
+        verbose_name="카카오 id",
+        max_length=255,
+        null=False,
+        primary_key=True,
+    )
+
+    profile_image = models.CharField(
+        verbose_name="프로필 사진",
+        max_length=255,
+        null=True,
+    )
+
+    real_name = models.CharField(
+        verbose_name="실명",
+        max_length=30,
+        null=False,
+    )
+
+    phone_number = models.CharField(
+        verbose_name="전화번호",
+        max_length=20,
+        null=False,
+    )
+
+    gender = models.IntegerField(
+        verbose_name="성별",
+        null=False,
+    )
+
+    birth_year = models.CharField(
+        verbose_name="생년월일",
+        max_length=20,
+        null=False,
+    )
