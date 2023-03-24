b9a88d67fa34a92d492908c470f45e58e6495550
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-03-24 22:01:51 +0900

    hotfix

--- webapp/user/urls.py
+++ webapp/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.user.views.kakaoUserView import kakaoCallback
+from user.views.kakaoUserView import kakaoCallback
 
 urlpatterns = [
     path('user/kakao/callback', kakaoCallback )
