eb30e53b655ccbb8ed87f7521bead62e82cab18d
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-03-27 16:46:50 +0900

    fix: surveyGifticon receiver 필드 attribute 수정

--- webapp/participant/admin/participantAdmin.py
+++ webapp/participant/admin/participantAdmin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from participant.models import Participant
 
+
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('id', 'kakaoId', 'survey', 'json',)
