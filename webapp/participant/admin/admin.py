1276e7d989da3e10b742355dcba4fa7381c01cce
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-03-27 11:13:02 +0900

    fix: participant admin 오타 수정

--- webapp/participant/admin/admin.py
+++ webapp/participant/admin/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from participant.models import Participant
 
 @admin.register(Participant)
-class GifticonAdmin(admin.ModelAdmin):
+class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('id', 'kakaoId', 'survey', 'json',)
