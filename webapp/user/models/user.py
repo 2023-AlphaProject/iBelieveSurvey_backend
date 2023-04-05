f8eb12a01f1abc3ed8e27409ff4f2807f2acf79a
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-04-05 21:00:07 +0900

    feat: BaseUserManager, AbstractBaseUser, PermissionMixin 연결

--- webapp/user/models/user.py
+++ webapp/user/models/user.py
@@ -3,6 +3,29 @@ from django.db import models
 from config.baseModel import BaseModel
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
+class UserManager(BaseUserManager):
+    """ModelManager definition for User Model"""
+
+    def _create_user(self, email, password, **kwargs):
+        user = self.model(
+            email=email,
+            **kwargs,
+        )
+        user.set_password(password)
+        user.save()
+
+    def create_user(self, email, password, **kwargs):
+        """일반 유저 생성 메소드"""
+        self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        """슈퍼 유저(superuser) 생성 메소드"""
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('realName', 'root')
+        kwargs.setdefault('phoneNumber', '010-0000-0000')
+        kwargs.setdefault('gender', True)
+        kwargs.setdefault('birth', '1900-01-01Z')
+        self._create_user(email, password, **kwargs)
 
 class User(AbstractBaseUser,PermissionsMixin):
 
@@ -14,16 +37,18 @@ class User(AbstractBaseUser,PermissionsMixin):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
-    kakaoId = models.CharField(
-        verbose_name="카카오 id",
-        max_length=255,
-        null=False,
-    )
+    # kakaoId = models.CharField(
+    #     verbose_name="카카오 id",
+    #     max_length=255,
+    #     null=False,
+    #     primary_key=True
+    # )
 
     email = models.CharField(
         verbose_name="카카오 이메일",
         max_length=255,
-        null=False,
+        null=True,
+        unique=True,
     )
 
     realName = models.CharField(
@@ -41,7 +66,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     gender = models.BooleanField(
         verbose_name="성별",
         null=False,
-        choices=gender
+        choices=gender,
     )
 
     birth = models.DateField(
@@ -50,5 +75,14 @@ class User(AbstractBaseUser,PermissionsMixin):
         null=False,
     )
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['', ]
+
+    @property
+    def is_staff(self):
+        return self.is_superuser
+
+    def __str__(self) -> str:
+        return f'[{self.realName} ({self.email})'
+
