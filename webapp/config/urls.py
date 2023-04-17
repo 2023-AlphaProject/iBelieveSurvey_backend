23523914ea38a6d4ee2fb86dc54e11f98e1196eb
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-04-17 16:25:24 +0900

    fix: urls의 settings.DEBUG 조건 삭제

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -40,9 +40,9 @@ urlpatterns = [
     path('user/', include('user.urls'), name='users'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    ]
+
+urlpatterns += [
+    re_path(r'^(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
