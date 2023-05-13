4b70c2395a2617b73f375f0e1abfc1c4b932bc27
koomchang <leahpar0401@gmail.com>
2023-05-14 03:20:47 +0900

    feat: base pagination 12개로 설정

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -62,6 +62,7 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': "%Y-%m-%d / %H:%M:%S",
     'EXCEPTION_HANDLER': 'config.exceptions.custom_exception_handler',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'config.pagination.DefaultPagination',
 }
 
 MIDDLEWARE = [
