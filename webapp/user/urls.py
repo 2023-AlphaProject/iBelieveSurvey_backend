4a65cdd9edc0e1c51dd809e5bc80e7b166e09f0f
koomchang <leahpar0401@gmail.com>
2023-05-26 03:43:19 +0900

    feat: mypage api 구현

--- webapp/user/urls.py
+++ webapp/user/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from user.views.kakaoUserView import kakaoCallback
 from user.views.kakaoUserView import kakaoGetLogin
+from user.views.myPagePaidAPIView import MyPagePaidAPIView
 from user.views.userAPIView import UpdateUserAPIView
+from user.views.myPageParticipatedAPIView import MyPageParticipatedAPIView
 
 urlpatterns = [
-    path('kakao/login',kakaoGetLogin),
+    path('kakao/login', kakaoGetLogin),
     path('kakao/callback', kakaoCallback, name="kakaoCallback"),
     path('update', UpdateUserAPIView.as_view()),
+    path('mypage/paid/', MyPagePaidAPIView.as_view()),
+    path('mypage/participated/', MyPageParticipatedAPIView.as_view()),
 ]
--- webapp/user/views/myPagePaidAPIView.py
+++ webapp/user/views/myPagePaidAPIView.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import action
 from rest_framework.views import APIView
 
 from config.pagination import DefaultPagination
@@ -6,21 +5,17 @@ from survey.models import Survey
 from survey.serializers import SurveySerializer
 
 
-class MyPageViewSet(APIView):
+class MyPagePaidAPIView(APIView):
     queryset = Survey.objects.all()
     serializer_class = SurveySerializer
     pagination_class = DefaultPagination
 
-    @action(detail=False, methods=['GET'])
-    def paid(self, request):
+    def get(self, request):
+        """
+        자신이 결제한 설문들을 반환합니다.
+        """
         surveys = Survey.objects.filter(writer=request.user, is_paid=True)
-        page = self.paginate_queryset(surveys)
-        serializer = SurveySerializer(page, many=True)
-        return self.get_paginated_response(serializer.data)
-
-    @action(detail=False, methods=['GET'])
-    def participated(self, request):
-        surveys = Survey.objects.filter(participants=request.user)
-        page = self.paginate_queryset(surveys)
-        serializer = SurveySerializer(page, many=True)
-        return self.get_paginated_response(serializer.data)
+        paginator = self.pagination_class()
+        paginated_surveys = paginator.paginate_queryset(surveys, request)
+        serializer = self.serializer_class(paginated_surveys, many=True)
+        return paginator.get_paginated_response(serializer.data)
