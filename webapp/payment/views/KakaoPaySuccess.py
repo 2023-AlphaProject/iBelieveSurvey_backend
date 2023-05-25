0f071870fec22c746d78c7c76876dd8518f7bb06
koomchang <leahpar0401@gmail.com>
2023-05-25 22:31:14 +0900

    fix: 카카오페이 결제 이후 survey status 수정 로직 변경

--- webapp/payment/views/KakaoPaySuccess.py
+++ webapp/payment/views/KakaoPaySuccess.py
@@ -8,8 +8,8 @@ from survey.models import Survey
 class KakaoPaySuccess(APIView):
     def get(self, request, survey_id):
         survey = Survey.objects.get(id=survey_id)
+        survey.is_idle = False
         survey.is_paid = True
-        survey.status = Survey.STATUS_CHOICES.IN_PROGRESS
         survey.started_at = timezone.now()
         survey.save()
         return redirect('http://localhost')
