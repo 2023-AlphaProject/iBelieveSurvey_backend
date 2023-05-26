7f922e4d790b7ffea4db08ecfdeb5332349809e8
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-05-26 13:53:45 +0900

    feat: build command 수정 - product_detail_url 필드가 추가된 template객체 생성

--- webapp/template/serializers/templateSerializer.py
+++ webapp/template/serializers/templateSerializer.py
@@ -7,13 +7,13 @@ class TemplateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Template
         fields = [
-            'pk', 'template_name', 'template_trace_id', 'order_template_status', 'budget_type',
-            'gift_sent_count', 'bm_sender_name', 'mc_image_url', 'mc_text', 'item_type', 'product_name',
-            'brand_name', 'product_image_url', 'product_thumb_image_url', 'brand_image_url', 'product_price'
+            'id', 'product_detail_url', 'template_name', 'template_trace_id', 'order_template_status',
+            'budget_type', 'gift_sent_count', 'bm_sender_name', 'mc_image_url', 'product_name', 'brand_name',
+            'product_image_url', 'product_thumb_image_url', 'brand_image_url', 'product_price'
         ]
         read_only_fields = [
-            'pk', 'template_name', 'template_trace_id', 'order_template_status', 'budget_type',
-            'gift_sent_count', 'bm_sender_name', 'mc_image_url', 'mc_text', 'item_type', 'product_name',
-            'brand_name', 'product_image_url', 'product_thumb_image_url', 'brand_image_url', 'product_price'
+            'id', 'product_detail_url', 'template_name', 'template_trace_id', 'order_template_status',
+            'budget_type', 'gift_sent_count', 'bm_sender_name', 'mc_image_url', 'product_name', 'brand_name',
+            'product_image_url', 'product_thumb_image_url', 'brand_image_url', 'product_price'
         ]
         # template_token을 제외한 모든 필드
