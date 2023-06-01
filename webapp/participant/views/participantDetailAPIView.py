545f8c726b01cf75b64a89d42a5086aae5382f1f
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-06-01 18:06:01 +0900

    feat: participant view 예외처리 수정

--- webapp/participant/views/participantDetailAPIView.py
+++ webapp/participant/views/participantDetailAPIView.py
@@ -19,7 +19,7 @@ class ParticipantDetailAPIView(RetrieveAPIView, UpdateAPIView):
         if not self.request.user.is_authenticated:
             return Response({"error": "설문에 답변하기 위해선 로그인이 필요합니다."})
 
-        if request.user != participant.user or request.user != self.get_survey().writer:
+        if request.user not in [participant.user, self.get_survey().writer]:
             return Response({"error": "설문 참여자 또는 설문 답변자만 해당 설문에 대한 답변을 조회할 수 있습니다."})
 
         return self.retrieve(request, *args, **kwargs)
