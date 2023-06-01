985092b7bf8e630aa5bc5b446c8c6d42726338a5
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-06-01 18:02:32 +0900

    feat: participant view 예외처리 추가

--- webapp/participant/views/participantDetailAPIView.py
+++ webapp/participant/views/participantDetailAPIView.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import RetrieveAPIView, UpdateAPIView
 from rest_framework.response import Response
+from survey.models import Survey
 
 from participant.models import Participant
 from participant.serializers.participantSerializer import ParticipantSerializer
@@ -18,8 +19,8 @@ class ParticipantDetailAPIView(RetrieveAPIView, UpdateAPIView):
         if not self.request.user.is_authenticated:
             return Response({"error": "설문에 답변하기 위해선 로그인이 필요합니다."})
 
-        if request.user != participant.user:
-            return Response({"error": "설문 참여자는 본인의 답변만 조회할 수 있습니다."})
+        if request.user != participant.user or request.user != self.get_survey().writer:
+            return Response({"error": "설문 참여자 또는 설문 답변자만 해당 설문에 대한 답변을 조회할 수 있습니다."})
 
         return self.retrieve(request, *args, **kwargs)
 
@@ -36,3 +37,8 @@ class ParticipantDetailAPIView(RetrieveAPIView, UpdateAPIView):
             return Response({"error": "설문 참여자는 본인의 답변만 수정할 수 있습니다."})
 
         return self.update(request, *args, **kwargs)
+
+    def get_survey(self):
+        survey_id = self.kwargs['survey_id']
+        survey = Survey.objects.get(id=survey_id)
+        return survey
