159d9c3609bfb9b4557a5e918cf1d790a28b7e8a
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-06-02 19:43:56 +0900

    feat: 선물발송 api

--- webapp/order/models/order.py
+++ webapp/order/models/order.py
@@ -3,6 +3,7 @@ from django.db import models
 from participant.models import Participant
 from survey.models import Survey
 from template.models.template import Template
+from cart.models import Cart
 
 
 # 아빌립서베이에 기프티콘 결제 요청오면 Cart의 quantity만큼 Order객체 생성
@@ -12,8 +13,9 @@ class Order(models.Model):
         verbose_name = "Order"
         verbose_name_plural = "Orders"
 
-    survey = models.ForeignKey(Survey, verbose_name="설문", on_delete=models.CASCADE, null=False)
-    template = models.ForeignKey(Template, verbose_name="템플릿", on_delete=models.CASCADE, null=False)
+    # survey = models.ForeignKey(Survey, verbose_name="설문", on_delete=models.CASCADE, null=False)
+    # template = models.ForeignKey(Template, verbose_name="템플릿", on_delete=models.CASCADE, null=False)
+    cart = models.ForeignKey(Cart, verbose_name="카트", on_delete=models.CASCADE, null=False)
     receiver = models.ForeignKey(Participant, verbose_name="템플릿 수신자", on_delete=models.CASCADE, null=False)
 
     def __str__(self):
