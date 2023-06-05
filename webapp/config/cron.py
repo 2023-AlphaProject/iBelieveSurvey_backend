8959d42d1b11ffe9dd1a663685447fd73be8ae50
koomchang <leahpar0401@gmail.com>
2023-06-05 20:52:42 +0900

    feat: 일정 시간 마다 중복 없이 랜덤한 당첨자 선발하여 db 저장

--- webapp/config/cron.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-def cron_test():
-    cnt = 0
-    print(str(cnt) + " : 찬진이형 바보")
-    print(datetime.now())
-    return
--- webapp/order/cronAssignReceiver.py
+++ webapp/order/cronAssignReceiver.py
@@ -1,23 +1,22 @@
 from datetime import datetime
 
-from cart.models import Cart
 from order.models import Order
 from participant.models import Participant
 
 
 def assignReciver():
     orders = Order.objects.filter(receiver=None)
-    if orders is None:
-        print("이미 모든 설문에 당첨자가 지정되어 있습니다.")
+    if orders.count() == 0:
+        print("이미 모든 설문에 당첨자가 지정되어 있습니다." + str(datetime.now()))
         return
     print("랜덤 당첨자 선정 시작 : " + str(datetime.now()))
+    print("--------------------------------------------")
+    exclude_receivers = []
     for order in orders:
-        cart_receivers = Order.objects.filter(cart=order.cart).values_list('receiver', flat=True)
-        print(cart_receivers)
-        available_receivers = Participant.objects.filter(survey=order.cart.survey).exclude(id__in=cart_receivers)
-        print(available_receivers)
+        available_receivers = Participant.objects.filter(survey=order.cart.survey).exclude(user__in=exclude_receivers)
         if available_receivers.exists():
             random_receiver = available_receivers.order_by('?').first()
+            exclude_receivers.append(random_receiver.user)
             order.receiver = random_receiver
             order.save()
             print("설문 : " + order.cart.survey.title)
@@ -26,6 +25,5 @@ def assignReciver():
             print("--------------------------------------------")
 
     print("랜덤 당첨자 선정 종료")
+    print("--------------------------------------------")
     return
-
-# 같은 cart에는 receiver가 중복되지 않도록 한다.
