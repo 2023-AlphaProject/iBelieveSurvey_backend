bf8fa55e24c4eab477997e6ac9869dfac6da4599
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-06-05 17:09:25 +0900

    feat: is_paid True로 변경될 시 불완전한 order객체들 생성

--- webapp/payment/views/KakaoPaySuccess.py
+++ webapp/payment/views/KakaoPaySuccess.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from rest_framework.views import APIView
 
+from cart.models import Cart
+from order.models.order import Order
 from survey.models import Survey
 
 
@@ -12,4 +14,15 @@ class KakaoPaySuccess(APIView):
         survey.is_paid = True
         survey.started_at = timezone.now()
         survey.save()
+
+        existing_orders = Order.objects.filter(survey=survey).count()
+
+        carts = Cart.objects.filter(survey=survey)
+
+        # 이미 order객체가 생성된 것이 없다면 모든 cart객체들의 quantity만큼 order객체들을 생성한다.
+        if existing_orders == 0:
+            for cart in carts:
+                for _ in range(cart.quantity):
+                    Order.objects.create(survey=survey, template=cart.template, receiver=None)
+
         return redirect('http://localhost')
