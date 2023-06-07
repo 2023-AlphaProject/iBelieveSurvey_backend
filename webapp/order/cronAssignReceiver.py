63778fe28c278edeebca53b7cf7fc03bce6f636d
koomchang <leahpar0401@gmail.com>
2023-06-07 23:27:56 +0900

    feat: receiver가 랜덤으로 만들어짐과 동시에 해당 receiver에게 선물 발송

--- webapp/order/cronAssignReceiver.py
+++ webapp/order/cronAssignReceiver.py
@@ -1,13 +1,13 @@
+import os
+import re
 from datetime import datetime
 
-from order.models import Order
-from participant.models import Participant
 import requests
 from rest_framework import status
-from rest_framework.response import Response
+
 from config.settings.base import SOCIAL_OUTH_CONFIG
-from user.models import User
-import os
+from order.models import Order
+from participant.models import Participant
 
 
 def assignReciver():
@@ -33,8 +33,11 @@ def assignReciver():
                 random_receiver = available_receivers.order_by('?').first()
                 exclude_receivers.append(random_receiver.user)
                 order.receiver = random_receiver
+                receiver = random_receiver.user
+                template = order.cart.template
+                if not sendGifticon(receiver, template):
+                    order.receiver = None
                 order.save()
-                sendGifticon(cart.uuid)
                 print("설문 : " + order.cart.survey.title)
                 print("당첨자 : " + random_receiver.user.realName)
                 print("기프티콘 : " + order.cart.template.template_name)
@@ -43,102 +46,76 @@ def assignReciver():
         else:
             print("설문 : " + order.cart.survey.title)
             print("해당 설문은 종료되지 않았기 때문에 당첨자를 지정하지 않고 다음 설문으로 넘어가 작업을 수행합니다.")
-
     print("랜덤 당첨자 선정 종료")
     print("--------------------------------------------")
     return
 
 
-def sendGifticon(uuid):
+def sendGifticon(receiver, template):
+    success_callback_url = "https://ibelievesurvey.com/"  # 일단 localhost로 고정 -> 나중에 수정 필요
+    fail_callback_url = "https://ibelievesurvey.com/"  # 일단 localhost로 고정 -> 나중에 수정 필요
+    receiver_type = "PHONE"
+    CLIENT_ID = SOCIAL_OUTH_CONFIG['KAKAO_REST_API_KEY']
+    template_token_list_str = os.environ.get("TEMPLATE_TOKEN_LIST")
+    template_token_list = eval(template_token_list_str)
+    print("--------------------------------------------")
+    print("선물 발송 시작 : " + str(datetime.now()))
 
-        success_callback_url = "https://ibelievesurvey.com/" # 일단 localhost로 고정 -> 나중에 수정 필요
-        fail_callback_url = "https://ibelievesurvey.com/" # 일단 localhost로 고정 -> 나중에 수정 필요
-        receiver_type = "PHONE"
-        CLIENT_ID = SOCIAL_OUTH_CONFIG['KAKAO_REST_API_KEY']
-        template_token_list_str = os.environ.get("TEMPLATE_TOKEN_LIST") 
-        template_token_list = eval(template_token_list_str)
-        print("토큰리스트~", template_token_list)
+    user = receiver
+    phone_number = receiver.phoneNumber
+    phone_number_pattern = r'^010-\d{4}-\d{4}$'
 
-        try:
-            # cart = Cart.objects.filter(is_sent=False)
-            order_list = Order.objects.filter(cart = uuid)
-            if len(order_list) == 0:
-                print("----------------------")
-                print("order 없음")
-                print("----------------------")
-                return Response({"error": "잘못된 장바구니 접근 입니다."}, status=400)
-            print(order_list)
+    if not re.match(phone_number_pattern, phone_number):
+        print("----------------------")
+        print("기프티콘 발송 실패 : 전화번호 형식이 맞지 않습니다.")
+        print("----------------------")
+        return False
 
-            for order in order_list:
-                receiver = order.receiver
-                cart = order.cart
+    real_name = user.realName
+    template_order_name = template.template_name
+    print("기프티콘 종류 : ", template_order_name)
+    print("받는 사람 : ", real_name)
+    print("휴대폰 번호 : ", phone_number)
 
-                kakaoId = receiver.user.kakaoId
-                template_id = cart.template_id
-                user = User.objects.get(kakaoId=kakaoId)
-                template = order.cart.template 
+    template_token = ""
+    for item in template_token_list:
+        if str(template_order_name) in item:
+            template_token = item[template_order_name]
+            break
 
-                phone_number = user.phoneNumber
-                if len(phone_number)==13 and phone_number[3]=="-" and phone_number[8]=="-": # 010-1234-5678 -> 13자
-                    phone_number = user.phoneNumber
-                else:
-                    formatted_number = phone_number[:3] + '-' + phone_number[3:7] + '-' + phone_number[7:]
-                    phone_number = formatted_number
+    payload = {
+        "template_token": template_token,
+        "receiver_type": receiver_type,
+        "receivers": [{
+            "name": real_name,
+            "receiver_id": phone_number
+        }],
+        "success_callback_url": success_callback_url,
+        "fail_callback_url": fail_callback_url,
+        "template_order_name": template_order_name,
+        "external_order_id": user.id,
+    }
 
-                real_name = user.realName
-                template_order_name = template.template_name
-                print("템플릿 이름", template_order_name)
-                print(real_name)
-                print(phone_number)
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "KakaoAK " + CLIENT_ID
+    }
 
-                for item in template_token_list:
-                    if str(template_order_name) in item:
-                        template_token = item[template_order_name]
-                        print("템플릿 토큰~",template_token)
-                        break
+    # 선물 발송 API 요청 보내기
+    response = requests.post("https://gateway-giftbiz.kakao.com/openapi/giftbiz/v1/template/order",
+                             json=payload, headers=headers)
 
-                template_trace_id = template.template_trace_id
-
-                payload = {
-                    "template_token": template_token,
-                    "receiver_type": receiver_type,
-                    "receivers": [{
-                        "name": real_name,
-                        "receiver_id": phone_number
-                    }],
-                    "success_callback_url": success_callback_url,
-                    "fail_callback_url": fail_callback_url,
-                    "template_order_name": template_order_name,
-                    "external_order_id": str(uuid) + str(order.id) 
-                }
-
-                headers = {
-                    "accept": "application/json",
-                    "content-type": "application/json",
-                    "Authorization": "KakaoAK " + CLIENT_ID
-                }
-
-                # 선물 발송 API 요청 보내기
-                response = requests.post("https://gateway-giftbiz.kakao.com/openapi/giftbiz/v1/template/order", json=payload, headers=headers)
-
-                if response.status_code != status.HTTP_200_OK:
-                    print("----------------------")
-                    print("기프티콘 발송 실패")
-                    print("----------------------")
-                    print("Request Payload:", payload)
-                    print("headers:", headers)
-                    print("Response Content:", response.content)
-                    return Response({"message": "선물 발송 요청 실패. " + response.text}, status=response.status_code)
-                    # return Response({"message": "선물 발송 요청 실패."}, status=response.status_code)
-                else:
-                    print("----------------------")
-                    print("기프티콘 발송 성공")
-                    print("----------------------")
-
-            return Response({"message": "선물 발송 요청 성공."}, status=status.HTTP_200_OK)
-
-        except (Order.DoesNotExist, User.DoesNotExist):
-            return Response({"message": "Required data not found."}, status=status.HTTP_404_NOT_FOUND)
-
-        except Exception as e:
-            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    if response.status_code != status.HTTP_200_OK:
+        print("----------------------")
+        print("기프티콘 발송 실패")
+        print("----------------------")
+        print("Request Payload:", payload)
+        print("headers:", headers)
+        print("Response Content:", response.content)
+        return False
+    else:
+        print("----------------------")
+        print("기프티콘 발송 성공")
+        print("----------------------")
+        return True
