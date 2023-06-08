078c7fe457a90ed54cba8344cd518497d4b5338e
koomchang <leahpar0401@gmail.com>
2023-06-08 14:24:52 +0900

    feat: api 호출 이후 redirect url 변경

--- webapp/payment/views/KakaoPayAPI.py
+++ webapp/payment/views/KakaoPayAPI.py
@@ -3,14 +3,21 @@ import os
 import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.conf import settings
 
 from cart.models import Cart
 from survey.models import Survey
+
+BASEURL = settings.BACKEND_URL
 
 
 class KakaoPayAPI(APIView):
 
     def post(self, request, survey_id):
+        """
+        카카오페이 결제 API를 호출합니다.
+        종료된 설문만 결제가 가능합니다.
+        """
         survey = Survey.objects.get(id=survey_id)
         if survey.end_at is None:
             return Response({'error': '설문 종료 일시가 설정되지 않았습니다.'}, status=400)
@@ -33,9 +40,9 @@ class KakaoPayAPI(APIView):
             'quantity': 1,
             'total_amount': total_amount,
             'tax_free_amount': 0,
-            'approval_url': f'http://localhost/surveys/{survey_id}/carts/payments/success',
-            'fail_url': 'http://localhost',
-            'cancel_url': 'http://localhost',
+            'approval_url': f'{BASEURL}surveys/{survey_id}/carts/payments/success',
+            'fail_url': f'{BASEURL}',
+            'cancel_url': f'{BASEURL}',
         }
         response = requests.post(url, headers=headers, params=params)
         result = response.json()
