048ce6b69ddc9de34f2625206438b3809baafcd3
koomchang <leahpar0401@gmail.com>
2023-06-08 13:12:18 +0900

    feat: order 관련 api 삭제

--- webapp/order/views/orderListAPIView.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.generics import ListAPIView
-
-from order.models import Order
-from order.serializers import OrderSerializer
-
-
-class OrderListAPIView(ListAPIView):
-    serializer_class = OrderSerializer
-    queryset = Order.objects.all()
