86b91846bb4b9875bc85dcde880fd43521051bf0
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-06-08 11:58:06 +0900

    fix: template 객체 생성 커맨드 수정

--- webapp/template/management/commands/template_objs_in_db.py
+++ webapp/template/management/commands/template_objs_in_db.py
@@ -1,67 +1,25 @@
+import ast
 import os
-
-import django
 import requests
 from django.core.management.base import BaseCommand
 
 from template.models import Template
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
-
-script_absol_dir = os.path.dirname(os.path.abspath(__file__))
-env_absol_dir = os.path.join(script_absol_dir, '../../../.env')
-
-product_detail_url_list = [
-    {'공차 밀크티': '6881256'},
-    {'할리스 에스프레소': '4653370'},
-    {'투썸플레이스 아메리카노': '4072511'},
-]
-template_token_list = []
-template_num_in_env = 3
-
 
 class Command(BaseCommand):
-    help = 'Create template objects and save in DB'
-
     def handle(self, *args, **options):
-        with open(env_absol_dir, "r", encoding="utf-8") as file:
+        template_token_dict_list_str = os.environ.get("TEMPLATE_TOKEN_LIST")
+        template_token_dict_list = ast.literal_eval(template_token_dict_list_str)
+        template_token_num = len(template_token_dict_list)
 
-            app_key_dict = {}
-
-            for line in file:
-                if line.startswith("#") or line.strip() == "":
-                    continue
-
-                elif line.startswith("KAKAO_GIFT_BIZ_REST_APP_KEY"):
-                    key, value = line.strip().split("=")
-                    value = value.strip("\'")
-                    app_key_dict[key] = value
-
-                elif line.startswith("TEMPLATE_TOKEN_LIST"):
-
-                    while True:
-                        line = file.readline().strip()
-
-                        if line.startswith("["):
-                            continue
-
-                        elif not line or line.endswith("]"):
-                            break
-
-                        else:
-                            template_token_dict = {}
-
-                            now_use_dict = line.strip("\r\n").strip(",")
-                            key, value = now_use_dict.strip("{").strip("}").split(":")
-                            key = key.strip("\'")
-                            value = value.strip("\'")
-
-                            template_token_dict[key] = value
-                            template_token_list.append(template_token_dict)
+        product_url_dict_list = [
+            {'공차 밀크티': '6881256'},
+            {'할리스 에스프레소': '4653370'},
+            {'투썸플레이스 아메리카노': '4072511'},
+        ]
 
         url = "https://gateway-giftbiz.kakao.com/openapi/giftbiz/v1/template"
-        api_key = "KakaoAK " + app_key_dict["KAKAO_GIFT_BIZ_REST_APP_KEY"]
+        api_key = "KakaoAK " + os.environ.get("KAKAO_GIFT_BIZ_REST_APP_KEY")
         headers = {
             "Authorization": api_key,
             "Accept": "application/json"
@@ -69,44 +27,51 @@ class Command(BaseCommand):
 
         response = requests.get(url, headers=headers)
         json_data = response.json()
-        total_count = json_data["totalCount"]
-        templates = json_data["contents"]  # list
+        templates = json_data["contents"]  # [{"":""}, {"":""}, {"": {"":""}, {"":""}}] 형태
 
         if Template.objects.exists():
+            if Template.objects.count() == template_token_num:
+                print("")
+                print("######################################################")
+                print("############ 빌드가 실행된 적이 있습니다. ############")
+                print("## DB 템플릿 객체 개수 == .env 파일의 환경변수 개수 ##")
+                print("###### 템플릿 객체 업데이트를 진행하지 않습니다. #####")
+                print("######################################################")
+                print("")
 
-            if Template.objects.count() == template_num_in_env:
-                print("template 객체 개수 = ", Template.objects.count())
-                print("env 속 template 객체 개수 = ", template_num_in_env)
-                print("빌드가 실행된 적이 있지만 템플릿 개수가 일치함 -> 템플릿 업데이트 불필요")
-                pass
+                template_objs = Template.objects.all()
+                for template_obj in template_objs:
+                    print(template_obj.template_token)
+                return
+
             else:
-                print("template 객체 개수 = ", Template.objects.count())
-                print("env 속 template 객체 개수 = ", template_num_in_env)
-                print("빌드가 실행된 적이 있지만 템플릿 개수가 일치하지 않음 -> 템플릿 업데이트 필요")
+                print("")
+                print("######################################################")
+                print("############ 빌드가 실행된 적이 있습니다. ############")
+                print("## DB 템플릿 객체 개수 != .env 파일의 환경변수 개수 ##")
+                print("######### 템플릿 객체 업데이트를 진행합니다. #########")
+                print("######################################################")
+                print("")
 
                 Template.objects.all().delete()
-
-                self.create_templates(templates, total_count)
-
         else:
-            print("template 객체 개수 = ", Template.objects.count())
-            print("env 속 template 객체 개수 = ", template_num_in_env)
-            print("빌드가 처음으로 실행")
-            self.create_templates(templates, total_count)
+            print("")
+            print("######################################################")
+            print("############ 빌드가 실행된 적이 없습니다. ############")
+            print("########### 템플릿 객체 생성을 진행합니다. ###########")
+            print("######################################################")
+            print("")
 
-    def create_templates(self, templates, total_count):
-        for n in range(0, total_count):
-            template = templates[n]
-            template_name = template["template_name"]
+        for template_token_dict, product_url_dict in zip(template_token_dict_list, product_url_dict_list):
 
-            for template_token_list_dict, product_detail_url_list_dict in zip(
-                    template_token_list, product_detail_url_list):
+            for template in templates:
+                template_name = template["template_name"]
 
-                if template_name in template_token_list_dict and product_detail_url_list_dict:
-                    product_detail_url = "https://gift.kakao.com/product/" + product_detail_url_list_dict[template_name]
+                if template_name in template_token_dict and template_name in product_url_dict:
+                    template_token = template_token_dict[template_name]
+                    product_detail_url = "https://gift.kakao.com/product/" + product_url_dict[template_name]
 
-                    template_token = template_token_list_dict[template_name]
-
+                    # 템플릿 정보에서 필요한 데이터 추출
                     template_trace_id = template["template_trace_id"]
                     order_template_status = template["order_template_status"]
                     budget_type = template["budget_type"]
@@ -125,16 +90,31 @@ class Command(BaseCommand):
                     brand_image_url = product_data["brand_image_url"]
                     product_price = product_data["product_price"]
 
-                    new_template = Template(product_detail_url=product_detail_url,
-                                            template_token=template_token, template_name=template_name,
-                                            template_trace_id=template_trace_id,
-                                            order_template_status=order_template_status,
-                                            budget_type=budget_type, gift_sent_count=gift_sent_count,
-                                            bm_sender_name=bm_sender_name, mc_image_url=mc_image_url,
-                                            mc_text=mc_text, item_type=item_type,
-                                            product_name=product_name, brand_name=brand_name,
-                                            product_image_url=product_image_url,
-                                            product_thumb_image_url=product_thumb_image_url,
-                                            brand_image_url=brand_image_url, product_price=product_price)
+                    # 템플릿 객체 생성 및 저장
+                    new_template = Template(
+                        product_detail_url=product_detail_url,
+                        template_token=template_token,
+                        template_name=template_name,
 
+                        template_trace_id=template_trace_id,
+                        order_template_status=order_template_status,
+                        budget_type=budget_type,
+                        gift_sent_count=gift_sent_count,
+                        bm_sender_name=bm_sender_name,
+                        mc_image_url=mc_image_url,
+                        mc_text=mc_text,
+
+                        item_type=item_type,
+                        product_name=product_name,
+                        brand_name=brand_name,
+                        product_image_url=product_image_url,
+                        product_thumb_image_url=product_thumb_image_url,
+                        brand_image_url=brand_image_url,
+                        product_price=product_price
+                    )
                     new_template.save()
+        print("")
+        print("######################################################")
+        print("### 템플릿 객체 생성 및 업데이트가 완료되었습니다. ###")
+        print("######################################################")
+        print("")
