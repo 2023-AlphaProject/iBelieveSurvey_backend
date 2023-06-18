db8f52d8bc92407eaef73d58c3c99a1bbaa32211
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-06-19 01:01:25 +0900

    fix: setting 실수 바로 잡기

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -183,10 +183,10 @@ BACKEND_URL = "https://api.ibelievesurvey.com/"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',  # 누구나 접근
-        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
