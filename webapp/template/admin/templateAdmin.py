996580fc7de937c534e912639e94941cab29b049
koomchang <leahpar0401@gmail.com>
2023-06-19 16:24:06 +0900

    fix: template admin 페이지 수정

--- webapp/template/admin/templateAdmin.py
+++ webapp/template/admin/templateAdmin.py
@@ -7,10 +7,9 @@ from template.models.template import Template
 class TemplateAdmin(admin.ModelAdmin):
     list_display = (
         'id',
-
-        'template_name', 'template_trace_id', 'order_template_status',
-        'budget_type', 'gift_sent_count', 'bm_sender_name', 'mc_image_url', 'mc_text',
-
-        'item_type', 'product_name', 'brand_name', 'product_image_url',
-        'product_thumb_image_url', 'brand_image_url', 'product_price'
+        'template_name',
+        'gift_sent_count',
+        'product_name',
+        'brand_name',
+        'product_price'
     )
